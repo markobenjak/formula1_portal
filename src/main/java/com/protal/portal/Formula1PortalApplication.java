7046d4d2432982efd1ff0c20fb457078c6f15e6a
mbenjak96 <marko.benjak@tvz.hr>
2021-12-02 20:58:49 +0100

    First commit

---
+++ src/main/java/com/protal/portal/Formula1PortalApplication.java
@@ -0,0 +1,13 @@
+package com.protal.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Formula1PortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Formula1PortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protal/portal/Formula1PortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protal.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formula1PortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
